8f4901f98f4cde07a1804082ea72473ef72a1f3d
Matt Parker <moonmaster9000@gmail.com>
2017-01-29 15:50:22 -0500

    MOAR contract

--- rps/spec/roundRepoSpec.js
+++ rps/spec/roundRepoSpec.js
@@ -1,68 +1,87 @@
 const generateUUID = require("uuid/v4")
 
-const FakeRoundRepo = function(){
+const FakeRoundRepo = function () {
     const rounds = []
 
-    this.save = function(round){
+    this.save = function (round) {
         rounds.push(round)
-        round.setId(generateUUID())
 
-        return promise(thenHandler => thenHandler())
-    }
-
-    this.getAll = function() {
-        return promise(thenHandler => thenHandler(rounds))
-    }
-
-    function promise(thenHandler) {
-        return {
-            then: thenHandler
+        if (round.getId() == null){
+            round.setId(generateUUID())
         }
+
+        return Promise.resolve(round)
+    }
+
+    this.getAll = function () {
+        return Promise.resolve(rounds)
     }
 }
 
-const Round = function({id}={}){
-    this.getId = function(){
+const Round = function ({id}={}) {
+    this.getId = function () {
         return id
     }
 
-    this.setId = function(newId){
+    this.setId = function (newId) {
         id = newId
     }
 }
 
 describe("round repo", function () {
+    let repo
+
+    beforeEach(function () {
+        repo = new FakeRoundRepo()
+    })
+
     it("saves rounds for later retrieval", function (done) {
-        const repo = new FakeRoundRepo()
-
         repo.save(new Round())
-
-            .then(function() {
-                return repo.getAll()
-            })
-
-            .then(function (rounds) {
+            .then(() =>
+                repo.getAll())
+            .then((rounds) => {
                 expect(rounds.length).toEqual(1)
                 done()
             })
     })
 
-    it("gives rounds a unique id", function(){
-        const repo = new FakeRoundRepo()
+    it("gives rounds ids on save", function (done) {
+        let round1 = new Round()
 
+        repo.save(round1).then((savedRound1) => {
+            expect(savedRound1.getId()).toBeDefined()
+            done()
+        })
+    })
+
+    it("honors provided ids on save", function (done) {
+        let round = new Round()
+        var roundId = "userprovidedid";
+        round.setId(roundId)
+
+        repo.save(round)
+            .then((savedRound) => {
+                expect(savedRound.getId()).toEqual(roundId)
+                done()
+        })
+    })
+
+    it("gives rounds a unique id", function (done) {
         let round1 = new Round()
         let round2 = new Round()
 
         repo.save(round1)
-            .then(()=>
+            .then(() =>
                 repo.save(round2))
-            .then(()=>
+            .then(() =>
                 repo.getAll())
             .then((rounds) => {
                 let ids = rounds.map(r => r.getId())
 
                 expect(ids.length).toEqual(2)
                 expect(ids[0]).not.toEqual(ids[1])
+
+                done()
             })
 
     })
